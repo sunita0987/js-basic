d1277fbbdda4a1ced8caab3a1790b1a4713b8566
sunita0987 <sunita0987546@gmail.com>
2025-02-19 15:11:41 +0530

    add array of object

--- assets/js/array.js
+++ assets/js/array.js
@@ -8,4 +8,46 @@ console.log(fruits[1]);//banana
 console.log(fruits.length);//length of array//
 
 
+/**
+ * array of object
+ */
+let myArr = [
+    {
+     studentName:"SUNITA",
+     studentid:589,
+     stuentNumber:854390902,
+     studentage:20
+    },
+    {studentName:"POOJA",
+    studentid:890,
+    studentNumber:785949034,
+    studentage:21    
+    },
+    {
+    studentName:"KHUSI",
+    studentid:940,
+    studentNumber:3454672348,
+    studentage:22
+    },
+    {
+    studentName:"PAYAL",
+    studentid:450,
+    studentNumber:7845673221,
+    studentage:23
+    },
+    {
+    studentName:"RANI",
+    studentid:346,
+    studentNumber:345689943,
+    studentage:25
+    }
+];
+
+console.log(myArr[0]);
+console.log(myArr[1]);
+console.log(myArr[2]);
+console.log(myArr[3]);
+console.log(myArr[4]);
+
+
 
